32e5c5f5a29c044cee8a0cc53c9a78a18637e856
leonardomg <leomonroy2017@gmail.com>
2019-07-23 00:54:18 -0500

    diseño con css

--- js/index.js
+++ js/index.js
@@ -1,35 +1,20 @@
-//consumo la api con  fetch
-fetch('https://rickandmortyapi.com/api/character')
-    .then(function(response) {
-        console.log('response.body =', response.body);
-        console.log('response.bodyUsed =', response.bodyUsed);
-        console.log('response.headers =', response.headers);
-        console.log('response.ok =', response.ok);
-        console.log('response.status =', response.status);
-        console.log('response.statusText =', response.statusText);
-        console.log('response.type =', response.type);
-        console.log('response.url =', response.url);
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        mostraPersonajes(data);
-    })
-    .catch(function(err) {
-        console.error(err);
-    });
+ traer();
+ //funcion que utiliza api fetch para obtener todos los personajes 
+ function traer() {
+     fetch('https://rickandmortyapi.com/api/character')
+         .then(function(response) {
+             return response.json();
+         })
+         .then(function(myJson) {
+             var divOut = document.getElementById('contenido');
+             var personaje = myJson.results; // llamamos al contenedor de todos los personajes
+
+             var txtOut = "";
+             for (var k in personaje) {
 
 
-//funcion para obtener personaje por pesonaje
-
-function mostraPersonajes(data) {
-    var result = 'result';
-    var resultado = ``;
-    for (var i in data) {
-        //objeto.hasOwnProperty se usa para filtrar las propiedades del objeto
-        if (data.hasOwnProperty(i)) {
-            resultado += `${resultr}.${i} = ${result[i]}\n`;
-        }
-    }
-    console.log("holaaaaaaa" + resultado)
-}
+                 txtOut += `<div class="col-lg-4 col-md-6 col-sm-12 animated  bounceInDown person" ><img src="${personaje[k].image}"><hr /> <b>${personaje[k].name}</b></div>`;
+             }
+             divOut.innerHTML = txtOut;
+         });
+ }
